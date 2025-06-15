a3d9fd8c7b792e410df538d693a5958b1564f2fb
Dominik Kiss <kissdominik7@gmail.com>
2025-06-15 10:22:12 +0200

    Add a class to persist data for future needs

---
+++ src/main/java/dev/domkss/persistance/GenericWorldData.java
@@ -0,0 +1,74 @@
+package dev.domkss.persistance;
+
+import net.minecraft.datafixer.DataFixTypes;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.world.PersistentState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.util.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+
+public class GenericWorldData extends PersistentState {
+    private static final String DATA_NAME = "underground_data";
+
+    public static final Type<GenericWorldData> TYPE = new Type<>(
+            GenericWorldData::new,
+            GenericWorldData::fromNbt,
+            DataFixTypes.SAVED_DATA_MAP_INDEX
+    );
+
+    private final Map<String, Object> data = new HashMap<>();
+
+    public GenericWorldData() {}
+
+
+    // Utility to fetch this data object
+    public static GenericWorldData get(ServerWorld world) {
+        return world.getPersistentStateManager().getOrCreate(TYPE, DATA_NAME);
+    }
+
+    // Save a key-value pair
+    public void saveData(Pair<String, Object> pair) {
+        data.put(pair.getLeft(), pair.getRight());
+        markDirty();
+    }
+
+    // Retrieve a value by key
+    public Object getDataByKey(String key) {
+        return data.get(key);
+    }
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
+        for (Map.Entry<String, Object> entry : this.data.entrySet()) {
+            Object value = entry.getValue();
+            if (value instanceof String s) nbt.putString(entry.getKey(), s);
+            else if (value instanceof Integer i) nbt.putInt(entry.getKey(), i);
+            else if (value instanceof Boolean b) nbt.putBoolean(entry.getKey(), b);
+            else if (value instanceof Double d) nbt.putDouble(entry.getKey(), d);
+        }
+        return nbt;
+    }
+
+    private static GenericWorldData fromNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup lookup) {
+        GenericWorldData data = new GenericWorldData();
+        for (String key : nbt.getKeys()) {
+            NbtElement element = nbt.get(key);
+            switch (Objects.requireNonNull(element).getType()) {
+                case NbtElement.STRING_TYPE -> data.data.put(key, nbt.getString(key));
+                case NbtElement.INT_TYPE -> data.data.put(key, nbt.getInt(key));
+                case NbtElement.BYTE_TYPE -> data.data.put(key, nbt.getBoolean(key));
+                case NbtElement.DOUBLE_TYPE -> data.data.put(key, nbt.getDouble(key));
+            }
+        }
+        return data;
+    }
+
+
+
+}
